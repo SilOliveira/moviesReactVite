43ba137546bba9f7f530ba0831953531fc1d0e9b
Julli Mayanne <jullimayanne9@gmail.com>
2023-07-17 19:21:11 -0300

    fix: endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import Home from './pages/Home/index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import Home from "./pages/Home/index.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,52 +1,49 @@
-import {Container, MovieList, Movie} from "./styles.jsx";
-import {useState, useEffect} from 'react'
+import { Container, MovieList, Movie } from "./styles.jsx";
+import { useState, useEffect } from "react";
 
-
-const api_key= import.meta.env.VITE_API_KEY;
-const image_path='https://api.themoviedb.org/3/movie/{movie_id}/images';
+const api_key = "api_key=18a04c2ea9f4244298d03e73a8ed901c";
+const image_path = `https://image.tmdb.org/t/p/w1280`;
 
 function Home() {
-  
-  const [movies, setMovies] = useState([])
+  // eslint-disable-next-line no-unused-vars
+  const [movies, setMovies] = useState([]);
 
+  function getMovies() {
+    fetch(`https://api.themoviedb.org/3/movie/popular?${api_key}`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results));
+  }
   useEffect(() => {
+    getMovies();
+  }, []);
 
-    fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`)
-    .then (response => response.json())
-    .then (data => setMovies (data.results))  
-  
-  
-  }, [])
-
-    
   return (
-
     <Container>
       <h1>Filmes mais populares</h1>
-     
+
       <MovieList>
-
-      {movies.map(movie => {
-        return (
-          <Movie key={movie.id}>
-            <a href="https://google.com.br"><img src={`${image_path}${movie.poster_path}`} alt={movie.title}/></a>
-            <span>{movie.title}</span>
-          </Movie>
-        )
-      })}
-
+        {movies.map((movie) => {
+          return (
+            <Movie key={movie.id}>
+              <a href="https://google.com.br">
+                <img
+                  src={`${image_path + movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          );
+        })}
       </MovieList>
-
     </Container>
-  
-  )
+  );
 }
 
 export default Home;
 
-
 // Informações do TMDB
-  // const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 // const url = 'https://api.themoviedb.org/3/movie/popular';
 // const options = {
