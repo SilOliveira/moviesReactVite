7bd849d032e4a438dc0f94a775469aa5cf3cfb6c
SilOliveira <ticaebeca@gmail.com>
2023-07-17 21:08:36 -0300

    alterado endereço das imagens

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,20 +3,23 @@ import {useState, useEffect} from 'react'
 
 
 const api_key= import.meta.env.VITE_API_KEY;
-const image_path='https://api.themoviedb.org/3/movie/{movie_id}/images';
+const image_path=`https://image.tmdb.org/t/p/w1280`;
 
 function Home() {
   
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
+  function getMovies(){
 
     fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`)
     .then (response => response.json())
-    .then (data => setMovies (data.results))  
+    .then (data => setMovies (data.results));
+  } 
   
-  
-  }, [])
+    useEffect(() => {
+     getMovies();
+
+  }, []);
 
     
   return (
@@ -29,7 +32,7 @@ function Home() {
       {movies.map(movie => {
         return (
           <Movie key={movie.id}>
-            <a href="https://google.com.br"><img src={`${image_path}${movie.poster_path}`} alt={movie.title}/></a>
+            <a href="https://google.com.br"><img src={`${image_path + movie.poster_path}`} alt={movie.title}/></a>
             <span>{movie.title}</span>
           </Movie>
         )
@@ -44,20 +47,3 @@ function Home() {
 
 export default Home;
 
-
-// Informações do TMDB
-  // const fetch = require('node-fetch');
-
-// const url = 'https://api.themoviedb.org/3/movie/popular';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzhjNTdkNjBjYWE0MDEwMjM1M2IxZjY0YWU4MWJlYSIsInN1YiI6IjY0YTljYjk1OWM5N2JkMDBjNWY3YzhjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FlcGEtBcNISl6_HuwXM8iYcLPl_IlvzgJOYwxuAui9Y'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error('error:' + err));
